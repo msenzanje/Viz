aaf2e9823194b118914d1ce351a9ec9b313a5fec
Maxwell Stansbery <maxwellstansbery@gmail.com>
2024-10-14 12:39:43 -0500

    add still image segmentation, .gitignore, requirements

---
+++ segmentation.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import random
+import cv2
+import numpy as np
+from datetime import datetime
+
+imgpath = "image1.jpg"
+
+model = YOLO("yolov8m-seg.pt")
+
+img = cv2.imread(imgpath)
+yolo_classes = list(model.names.values())
+classes_ids = [yolo_classes.index(clas) for clas in yolo_classes]
+
+conf = 0.5
+
+results = model.predict(img, conf=conf)
+colors = [random.choices(range(256), k=3) for _ in classes_ids]
+print(results)
+
+for result in results:
+    for mask, box in zip(result.masks.xy, result.boxes):
+        points = np.int32([mask])
+        # cv2.polylines(img, points, True, (255, 0, 0), 1)
+        color_number = classes_ids.index(int(box.cls[0]))
+        cv2.fillPoly(img, points, colors[color_number])
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+cv2.imwrite("res", img)
